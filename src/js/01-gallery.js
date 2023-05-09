016fdcbd1db88098eba031ad85c4b81d53e0c298
Oleg Furculita <Oleg.Furculita1408@gmail.com>
2023-05-09 19:41:09 +0300

    01 tz

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+
+const galleryUl = document.querySelector('.gallery');
+
+function gallery(arr) {
+    return arr.map((items) => markup(items));
+  };
+  function markup(items) {
+    const {preview, original, description} = items;
+    return `<li class="gallery__item">
+              <a class='gallery__link' href='${original}'>
+                <img class='gallery__image'
+                      src='${preview}'
+                      data-source='${original}'
+                      alt='${description}'
+                />
+              </a>
+            </li>`;
+  };
+  
+  function renderList() {
+    gallery(galleryItems);
+    galleryUl.innerHTML = gallery(galleryItems).join("");
+  };
+  renderList();
+  
+  new SimpleLightbox(".gallery a", {captionDelay: 250, overlayOpacity: 1, captionsData: "alt"});
